17e40ac7c0b55d39bcfccfea6757bef757d11631
MetinAgayev <matinga@code.edu.az>
2021-12-19 16:22:10 +0400

    Add project files.

---
+++ ConsoleApp27/NewFolder/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp27.NewFolder
+{
+   public static class Class1
+    {
+        public static bool Word(this string word)
+        {
+            string[] letter = word.Split(' ');
+
+            if (word == "")
+            {
+                return false;
+            }
+            for (int i = 0; i < letter.Length; i++)
+            {
+                if (i == 0)
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,14 @@
+﻿using ConsoleApp27.NewFolder;
+using System;
+
+namespace ConsoleApp27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "13";
+            Console.WriteLine(word.Word());
+        }
+    }
+}
